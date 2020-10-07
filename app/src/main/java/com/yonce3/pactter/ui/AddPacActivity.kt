bdedb58098b5ccb756c2336c94d0c67c3d311d07
yonce3 <k0914t_u@icloud.com>
2020-10-07 22:49:19 +0900

    【作業中】カメラ機能

--- app/src/main/java/com/yonce3/pactter/ui/AddPacActivity.kt
+++ app/src/main/java/com/yonce3/pactter/ui/AddPacActivity.kt
@@ -1,13 +1,20 @@
 package com.yonce3.pactter.ui
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.room.Room
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yonce3.pactter.R
 import com.yonce3.pactter.data.AppDatabase
 import com.yonce3.pactter.data.entity.Pac
@@ -22,6 +29,9 @@ class AddPacActivity : AppCompatActivity() {
     lateinit var addPacButton: Button
     lateinit var backButton: ImageView
     lateinit var pacText: EditText
+    lateinit var cameraButton: FloatingActionButton
+    val REQUEST_IMAGE_CAPTURE = 1
+    val REQUEST_PERMISSION = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,5 +63,34 @@ class AddPacActivity : AppCompatActivity() {
         inputMethodManager.hideSoftInputFromWindow(pacText.windowToken, 0)
         inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS)
         //inputMethodManager.showSoftInput(pacText, 0)
+
+        cameraButton = findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener {
+            val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION) // REQUEST_PERMISSION は定数
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    // TODO: 撮影したDataを受け取る
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun startCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
     }
 }
